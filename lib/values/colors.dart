de44458d2a19749579194f81dc0789c55b5e0196
Felipe de Luca <felipedelucanogueira@gmail.com>
2022-06-14 18:51:18 -0300

    project dependencies and assets added

---
+++ lib/values/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const primary = Color(0xFF1CE394);
+  static const primaryDark = Color(0xFF0F8170);
+  static const secondary = Color(0xFFFFC107);
+  static const blackBackground = Color(0xFF0F1414);
+  static const white = Color(0xFFFFFFFF);
+  static const black = Color(0xFF000000);
+}
---
+++ lib/values/strings.dart
@@ -0,0 +1,30 @@
+abstract class Strings {
+  static const title = 'Chornos Partners';
+  static const chronos = 'Chronos';
+  static const partners = 'Partners';
+  static const splashFooter =
+      '© Chronos Partners - Todos os direitos reservados';
+
+  //register and login
+  static const nameHint = 'Nome';
+  static const emailHint = 'Email';
+  static const passwordHint = 'Senha';
+  static const cellphoneHint = 'Telefone';
+  static const registryHint = 'Codigo de Registro';
+
+  static const confirmPasswordHint = 'Confirmar senha';
+  static const register = 'Cadastre-se';
+  static const forgotPassword = 'Esqueci minha senha';
+
+  static const userTermsPrefix =
+      'ao cadastrar o usuário estará concordando com os';
+  static const userTerms = 'termos de uso do aplicativo';
+  static const userTermsSufix = 'disponiveis no site oficial';
+
+  //forgot password
+  static const resetPasswordTitle =
+      'Infome o e-mail cadastrado em sua conta para receber uma nova senha temporária.';
+  static const resetPasswordSubtitle =
+      'Não esqueça de checar também a caixa de spam de seu e-mail.';
+  static const resetPasswordButton = 'Recuperar';
+}
